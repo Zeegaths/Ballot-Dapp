a3e6fc60f2115a64e1ce11ab9a2aa40dcbb67a3c
Zeegaths <zarahgathoni76@gmail.com>
2024-03-09 15:27:35 +0100

    flex

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -4,13 +4,16 @@ import useIsChairPerson from "../hooks/useIsChairPerson";
 
 export default function Header() {
     const isChairPerson = useIsChairPerson();
-    return (
-        <div className="flex justify-between items-center">
+    return(
+        
+            <Flex gap={"4"} align={"center"} justify={"between"}>
             <div>Ballot</div>
             <Flex gap={"4"} align={"center"}>
+
                 {isChairPerson && <GiveRightToVoteComponent />}
                 <w3m-button />
             </Flex>
-        </div>
+            </Flex>
+        
     );
 }
